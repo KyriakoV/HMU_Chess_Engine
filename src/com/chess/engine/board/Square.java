525f16fb86022bb9ef344530381e67c36684d84f
KyriakoV <kyriakoseyxe@gmail.com>
2022-04-10 18:39:33 +0300

    Adding comments.intializing BoardTools class.

--- src/com/chess/engine/board/Square.java
+++ src/com/chess/engine/board/Square.java
@@ -7,30 +7,26 @@ import com.chess.engine.pieces.Piece;
 public abstract class Square {
     protected final int squareCoordinate;
 
-    private static final Map<Integer,EmptySquare> EMPTY_SQUARE_CACHE = createAllEmptySquares();
-    private static Map<Integer, EmptySquare> createAllEmptySquares() {
+    private static final Map<Integer,EmptySquare> EMPTY_SQUARE_CACHE = createAllEmptySquares();// a map to contain all empty squares
+    private static Map<Integer, EmptySquare> createAllEmptySquares() {//creates a map of all possible empty squares
         final Map<Integer,EmptySquare> emptySquareMap = new HashMap<>();
-        for (int i = 0; i < 64; i++) {
+        for (int i = 0; i < 64;i++) {
             emptySquareMap.put(i, new EmptySquare(i));
             return emptySquareMap;
         }
         return null;
     }
-    
-
-public static Square createSquare(final int squareCoordinate,final Piece piece) {
-    return piece != null ? new OccupiedSquare(squareCoordinate, piece) : EMPTY_SQUARE_CACHE.get(squareCoordinate);
+public static Square createSquare(final int squareCoordinate,final Piece piece) {//creates a square
+    return piece != null ? new OccupiedSquare(squareCoordinate, piece) : EMPTY_SQUARE_CACHE.get(squareCoordinate);//either returns an occupied square or points you to an empty squers position
 }
-
-
    private Square(int squareCoordinate) {
         this.squareCoordinate = squareCoordinate;
     }
-    public abstract boolean isOccupied();
+    public abstract boolean isOccupied();//method that checks if a square is occupied
 
-    public abstract Piece getPiece();
+    public abstract Piece getPiece();//a getter method for a piece
 
-    public static final class EmptySquare extends Square {
+    public static final class EmptySquare extends Square {// a subClass of Square refering to an empty square. 
         private EmptySquare(final int coordinate){
             super(coordinate);
         }
@@ -43,10 +39,8 @@ public static Square createSquare(final int squareCoordinate,final Piece piece) 
             return null;
         }
     }
-    public static final class OccupiedSquare extends Square {
-        
+    public static final class OccupiedSquare extends Square {//a subClass of Square refering to an occupied square.
         private final Piece pieceOnSquare;
-        
        private OccupiedSquare(int squareCoordinate,Piece pieceOnSquare){
             super(squareCoordinate);
             this.pieceOnSquare = pieceOnSquare;
@@ -60,6 +54,4 @@ public static Square createSquare(final int squareCoordinate,final Piece piece) 
             return this.pieceOnSquare;
         }
     }
-
-
 }
